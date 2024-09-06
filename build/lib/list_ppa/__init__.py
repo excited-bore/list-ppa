466941320e0ce335a0c3be29adabac4d1c92370e
excited-bore <stan96@duck.com>
2024-09-06 12:03:31 +0200

    Fixes n stuff

--- build/lib/list_ppa/__init__.py
+++ build/lib/list_ppa/__init__.py
@@ -1,5 +1,8 @@
 import os
 
+from shutil import which
+import subprocess
+
 from prompt_toolkit import prompt
 from prompt_toolkit.completion import WordCompleter
 from prompt_toolkit.completion import PathCompleter
@@ -14,6 +17,8 @@ import requests
 import re
 
 from bs4 import BeautifulSoup
+
+check_av=False
 
 def prompt_autocomplete():
     app = get_app()
@@ -33,8 +38,18 @@ def get_ppas_and_archive(url):
         if href and href.startswith('/~'):
             user = str(href.split('/')[1]).replace('~', '') 
             pack = str(href.split('/ubuntu')[1]) 
-            ppas.append(user+pack)
-             
+            if which('add-apt-repository') is not None and check_av == True:
+                codename=subprocess.check_output(["lsb_release", "-sc"])
+                codename=str(codename).replace('b', '').replace('\\n','').replace("'","",2) 
+                url="http://ppa.launchpad.net/" + str(user) + str(pack) + "/ubuntu/dists/" + str(codename) + "/"
+                print(url) 
+                response = requests.get(url)
+                if response.status_code == 200:
+                    ppas.append(user+pack)
+                else:
+                    continue
+            else:
+                   ppas.append(user+pack)
     return ppas
 
 
@@ -44,37 +59,39 @@ def main():
     choices = argcomplete.completers.ChoicesCompleter
     parser = argparse.ArgumentParser(description="List available ppas from 'https://launchpad.net' and add results to a file (if not in file already)",formatter_class=argparse.ArgumentDefaultsHelpFormatter)
     parser.add_argument("-o", "--only-list", action='store_true', required=False, help="Only list configuration")
-    parser.add_argument("-f","--file", required=False, help="Output file", metavar="Output file")
+    parser.add_argument("-f","--file", required=False, help="Output file", metavar="Output file", default=def_path)
+
+    if which('add-apt-repository') is not None:
+        parser.add_argument("-n","--not-check-available", action='store_false', required=False, help="Dont check if available for Ubuntu version")
+         
     
     output_stream = None
     if "_ARGCOMPLETE_POWERSHELL" in os.environ:
         output_stream = codecs.getwriter("utf-8")(sys.stdout.buffer)
 
     argcomplete.autocomplete(parser, output_stream=output_stream)
-
+    
     args = parser.parse_args()
    
      
     # Dirty method but it does the trick 
     alphabet=['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0']
     
-    
     path=def_path
     
-    print(args) 
-   
     res='n' 
     
-    if not args.file and args.only_list == False: 
+    if hasattr(args, 'not_check_available'): 
+        check_av=args.not_check_available 
+
+    if args.file == def_path and args.only_list == False: 
         res = prompt("Save results to file or output only? [y/n]: ", pre_run=prompt_autocomplete, completer=WordCompleter(["y", "n"]))
-    elif args.file:
+    elif not args.file == def_path:
         res='y'
     elif args.only_list == True:
         res='n'
 
-    print(res) 
     if res == 'y':
-        
         if not args.file: 
             res1 = prompt("Filepath: (can be nonexistant - Empty: " + str(def_path) + "): ", pre_run=prompt_autocomplete, completer=PathCompleter())
         else:
